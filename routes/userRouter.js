3bb0cb3ef910a267ede04fd20e645a1bb67cc694
Maxell Milay <milaymaxell@gmail.com>
2022-07-23 23:34:40 +0800

    [Adjustment][Maxell] fixed merge conflict

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,10 @@
-import express from 'express';rs/u
-import { logIn, signUp, testJWT } from '../controlleserController.js';
+import express from "express";
+import { logIn, signUp, testJWT } from "../controlleserController.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/sign-up', signUp);
-userRouter.post('/login', logIn);
-userRouter.post('/testJWT', testJWT);
+userRouter.post("/sign-up", signUp);
+userRouter.post("/login", logIn);
+userRouter.post("/testJWT", testJWT);
 
 export default userRouter;
